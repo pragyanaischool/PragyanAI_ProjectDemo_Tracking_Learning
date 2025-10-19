f723c12bd2e02091b40121a79780070888098355
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2025-10-19 20:40:47 +0530

    Update PragyanAI_ProjectDemo_Tracking_App.py

--- PragyanAI_ProjectDemo_Tracking_App.py
+++ PragyanAI_ProjectDemo_Tracking_App.py
@@ -13,7 +13,9 @@ from langchain_community.document_loaders import WebBaseLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain.chains import create_retrieval_chain
+#from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
+
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 # --- LLM & RAG Imports ---
